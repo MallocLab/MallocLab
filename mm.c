412bf6b89b218c6bbdf54f58879ccafc68f6bbda
Andrew Ko <ako5@cs.binghamton.edu>
2013-08-01 20:19:46 -0400

    Added Constants and macros and modified mm_init and mm_free. Also added extend_heap and coalesce functions

--- mm.c
+++ mm.c
@@ -44,12 +44,52 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* Basic constants and macros */
+#define WSIZE		4	/* Word and header/footer size (bytes) */
+#define DSIZE		8	/* Double word size (bytes) */
+#define CHUNKSIZE	(1<<12) /* Extend heap by this amount (bytes) */
+
+#define MAX(x, y) ((x) > (y)? (x)  : (y))
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* REad and write a word at address p */
+#define GET(p) 		(*(unsigned int *)(p))
+#define PUT(p, val) 	(*(unsigned int *)(p) = (val))
+
+/* Read the size and allocated fields from address p */
+#define GET_SIZE(p)	(GET(p) & ~0x7)
+#define GET_ALLOC(p)	(GET(p) & 0x1)
+
+/* Givenblock ptr bp, compute address of its header and footer */
+#define HDRP(bp)	((char *)(bp) -WSIZE)
+#define FTRP(bp)	((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)	((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)	((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+static char *heap_listp = 0;
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
-    return 0;
+	/* Create the initial empty heap */
+	if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+		return -1;
+	PUT(heap_listp, 0)
+	PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+	PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+	heap_listp += (2*WSIZE);
+
+	/* Extend the empty heap with a free block of CHUNKSIZE bytes */
+	if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+		return -1;
+        return 0;
 }
 
 /* 
@@ -95,12 +135,58 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+	size = (words % 2) ? (words + 1) * WSIZE: words * WSIZE;
+	if ((long)(bp = mem_sbrk(size)) == -1)
+	{
+		return NULL;
+	}
+	PUT(HDRP(bp), PACK(size, 0));
+	PUT(FTRP(bp), PACK(size, 0));
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
+	return coalesce(bp);
+}
 
+static void *coalesce(void *bp)
+{
+        size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+        size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+        size_t size =GET_SIZE(HDRP(bp));
 
+        if (prev_alloc && next_alloc)				/* Case 1 */
+        {
+                return bp;
+        }
 
+        else if (prev_alloc && !next_alloc)			/* Case 2 */
+        {
+                size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+                PUT(HDRP(bp), PACK(size, 0));
+                PUT(FTRP(bp), PACK(size, 0));
 
+        }
 
+        else if (!prev_alloc &&next_alloc)			/* Case 3 */
+        {
+                size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+                PUT(FTRP(bp), PACK(size, 0));
+                PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+                bp = PREV_BLKP(bp);
+	}
+
+	else 							/* Case 4 */
+	{
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP;
+	}
+	return bp;
+}
 
 
 
